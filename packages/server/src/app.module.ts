dfc0c75ad80f2d065e6e266dc5071dbecfd93566
Christos Georgiou <georgiou.christos@outlook.com>
2020-11-20 09:20:26 +0200

    add logger

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -1,24 +1,19 @@
-import { CacheModule, MiddlewareConsumer, Module, NestModule } from "@nestjs/common";
+import { CacheModule, Module } from "@nestjs/common";
 import { GeoipController } from "./controllers/geoip.controller";
 import { HealthController } from "./controllers/health.controller";
 import { OgController } from "./controllers/og.controller";
 import { ScreenshotsController } from "./controllers/screenshot.controller";
-import { AnalyticsMiddleware } from "./middlewares/analytics.middleware";
 
 @Module({
-  imports: [CacheModule.register()],
+  imports: [
+    CacheModule.register(),
+  ],
   controllers: [
     GeoipController,
     OgController,
     ScreenshotsController,
-    HealthController
+    HealthController,
   ],
   providers: [],
 })
-export class AppModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer
-      .apply(AnalyticsMiddleware)
-      .forRoutes("/");
-  }
-}
+export class AppModule { }
--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -1,12 +1,45 @@
-import { NestFactory } from '@nestjs/core';
-import * as helmet from 'helmet';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import * as helmet from "helmet";
+import { utilities as nestWinstonModuleUtilities, WinstonModule } from "nest-winston";
+import * as winston from "winston";
+import { AppModule } from "./app.module";
+
+const transports = [
+  new winston.transports.File({
+    filename: './logs/combined.log',
+    level: 'info'
+  }),
+  new winston.transports.File({
+    filename: './logs/errors.log',
+    level: 'error'
+  }),
+  new winston.transports.Console({
+    format: winston.format.combine(
+      winston.format.timestamp(),
+      nestWinstonModuleUtilities.format.nestLike(),
+    ),
+  }),
+]
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: WinstonModule.createLogger({
+      transports
+    })
+  });
+  
   app.use(helmet());
   app.enableCors();
-  app.setGlobalPrefix('v1');
+  app.setGlobalPrefix("v1");
+
+  const options = new DocumentBuilder()
+    .setTitle('Tools')
+    .setVersion('0.1')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
